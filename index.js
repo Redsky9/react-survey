f71abca0fd85a58eff304d2d4a22d7e914a39018
unknown <casca_de_fier@yahoo.com>
2017-09-09 10:02:25 +0300

    finished auth flow

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-var express = require('express');
+var express = require("express");
+var mongoose = require("mongoose");
+var cookieSession = require("cookie-session");
+var passport = require("passport");
+var keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
 var app = express();
-var port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send({bye: 'bye birdie'});
-});
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.listen(port, () => {
-    console.log("Server started!");
+mongoose.connect(keys.mongoURI);
+
+require("./routes/authRoutes")(app);
+
+var PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server has started on port " + PORT);
 });
